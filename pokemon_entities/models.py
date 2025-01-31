04e2237f74b395b9806259492aaf48f1129aa5f6
VASILIYKAS <yoyoynv@gmail.com>
2025-01-31 17:52:57 +0500

    Add evolution to db models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,16 +1,27 @@
 from django.db import models  # noqa F401
 
 # your models here
+
+
 class Pokemon(models.Model):
     title_ru = models.CharField(max_length=200)
     title_en = models.CharField(max_length=200, blank=True)
     title_jp = models.CharField(max_length=200, blank=True)
     image = models.ImageField(null=True)
     description = models.TextField(null=True, blank=True)
+    previous_evolution = models.ForeignKey('self',
+                                       on_delete=models.SET_NULL,
+                                       null=True, 
+                                       blank=True,
+                                       related_name='next_evolutions',
+                                       verbose_name='Из кого эволюционирует'
+                                       )
+
 
     def __str__(self):
         return self.title_ru
-    
+
+
 class PokemonEntity(models.Model):
     name = models.ForeignKey(Pokemon, on_delete=models.CASCADE, default=1)
     lat = models.FloatField(null=True, blank=True)
